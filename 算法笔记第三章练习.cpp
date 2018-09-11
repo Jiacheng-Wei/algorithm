316e367ee5adc0a1b759c7766a2e357962c702cb
Jiacheng Wei <982957368@qq.com>
2018-09-11 18:25:16 +0800

    9.11

--- 算法笔记第三章练习.cpp
+++ 算法笔记第三章练习.cpp
@@ -830,3 +830,51 @@ int main()
 	return 0;
  } 
  */
+ /*
+ 第二题：
+ 题目描述
+输入一个数n，然后输入n个数值各不相同，再输入一个值x，输出这个值在这个数组中的下标（从0开始，若不在数组中则输出-1）。
+
+输入
+测试数据有多组，输入n(1<=n<=200)，接着输入n个数，然后输入x。
+
+输出
+对于每组输入,请输出结果。
+
+样例输入
+4
+1 2 3 4
+3
+样例输出
+2 
+
+#include <stdio.h> 
+const int maxn=210;
+int a[maxn];
+int main()
+{
+	int n,x;
+	while (scanf("%d",&n)!=EOF)
+	{
+		for (int i=0;i<n;i++)
+		{
+			scanf("%d",&a[i]); 
+		}
+		scanf("%d",&x);
+		int k=0; //下标
+		for (k;k<n;k++)
+		{
+			if (a[k]==x)
+			{
+				printf("%d\n",k);
+				break;
+			}
+		} 
+		if (k==n) 
+		{
+			printf ("-1\n");
+		}
+	}
+	return 0;
+} */
+
