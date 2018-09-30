133d95705d1b35bbaaa8fd31b62bd63754251ad2
Jiacheng Wei <982957368@qq.com>
2018-10-01 00:06:20 +0800

    10.1

--- 第三章代码/算法笔记第三章第6节.cpp
+++ 第三章代码/算法笔记第三章第6节.cpp
@@ -9,12 +9,12 @@
 可能有多组测试数据，对于每组数据，
 不借用任何字符串库函数实现无冗余地接受两个字符串，然后把它们无冗余的连接起来。
 输出连接后的字符串。 
-*/ 
+ 
 #include<stdio.h>
 #include<string.h>
 int main()
 {
-	char str1[100],str2[100];
+	char str1[101],str2[101];
 	int lenth,lenth1,lenth2;
 	while(scanf("%s %s",str1,str2)!=EOF)
 	{
@@ -27,11 +27,44 @@ int main()
 		{
 			str[i]=str1[i];
 		}
-		for(j=0;j<lenth2;j++,i++)
+		for(j=0;j<lenth2&&i<lenth;j++,i++)
 		{
 			str[i]=str2[j];
 		}
-		printf("%s",str);
+		//str[lenth]='\0';//要用printf（"%s"）输出时必须要加上"\0"的字符串结束符，否则字符串输出会遇到问题，在oj上不能通过
+		//strlen返回的字符串长度不包括'\0'的长度 
+		//printf("%s\n",str);
+		for(int k=0;k<lenth;k++)
+		{
+			printf("%c",str[k]);
+		}//如果不采用 printf（"%s"）形式输出，就不用加 str[lenth]='\0'，oj能通过 
+		printf("\n");
 	}
 	return 0;
 }
+
+//below is example 
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int lena,lenb,i;
+    char a[101];
+    char b[101];
+    char c[210];
+    while((scanf("%s %s",a,b))!=EOF)
+    {
+        lena=strlen(a);
+        lenb=strlen(b);
+        for(i=0; i<lena+lenb; i++)
+        {
+            if(i<lena)  c[i]=a[i];
+            else   c[i]=b[i-lena];
+        }
+        c[lena+lenb]='\0';
+        printf("%s\n",c);
+    }
+    return 0;
+}
+*/
